32b8ea6d7d3ec2f7e08621cfa8bca6af67741afd
Amanda Ariyaratne <amanda.ariyaratne@protonmail.com>
2021-10-20 12:41:19 +0530

    fix artist name

--- corpus/translator.py
+++ corpus/translator.py
@@ -1,6 +1,5 @@
 import time
 from selenium import webdriver
-import selenium 
 import json
 from tqdm import tqdm
 
@@ -12,12 +11,17 @@ lang_code = 'si'
 browser = webdriver.Chrome("/home/aari/Projects/web-scraping/chromedriver")
 
 for i in tqdm(range(len(data))):
+
+    # name
     source_text = data[i]["name"]
     if ord(source_text[0]) < 200:
         browser.get("https://translate.google.co.in/?sl=en&tl="+lang_code+"&text="+source_text+"&op=translate")
         time.sleep(8)
         target_text = browser.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[1]').text
         data[i]["name"] = target_text
+
+    # birth
+    data[i]["birth"].replace("January", "ජනවාරි")
 
 with open('artists_translated.json', 'a') as f:
     f.write("%s,\n" % data)
